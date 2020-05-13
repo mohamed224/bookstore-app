f59c914e0757a7a520373f0f7f085096b4d0b1b8
Mohamed DIABY <mdiaby@spark.local>
2020-05-13 22:50:04 +0100

    initial commit

---
+++ src/test/java/tn/enis/bookstrore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.enis.bookstrore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
